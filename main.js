e2173a357362621b1e5e1dcc64d8615b6e64d0a2
UZB-AZIMO <abduvorisazimov712@gmail.com>
2024-07-07 02:17:13 +0500

    figma

---
+++ main.js
@@ -0,0 +1,24 @@
+const tilte = document.querySelector(".omad__title")
+const omad__btn = document.querySelector(".omad__btn")
+
+
+const TEL__NUMBER =[
+    "+998 99 825 55 19",
+    "+998 99 825 56 13",
+    "+998 99 825 45 18",
+    "+998 99 825 95 16",
+    "+998 99 825 52 17",
+    "+998 99 825 21 15" ,
+]
+
+omad__btn.addEventListener("click",()=>{
+    omad__btn.setAttribute("disabled",true)
+    let interval= setInterval(()=>{
+        let index = Math.floor(Math.random()*TEL__NUMBER.length )
+        tilte.textContent = TEL__NUMBER [index]
+    },100)
+setInterval(()=>{
+    clearInterval(interval)
+    omad__btn.removeAttribute("disabled")
+}, 2500)
+})
