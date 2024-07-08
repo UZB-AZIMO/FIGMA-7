260a6c4acb9fa849521751a903c34aa5b7be5f82
UZB-AZIMO <abduvorisazimov712@gmail.com>
2024-07-08 10:14:37 +0500

    figma

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 const sidebar = document .querySelector(".sidebare")
 const btns = document.querySelector(".btns")
 const but = document.querySelector(".but")
-
-
+const header = document.querySelector("header")
 btns.addEventListener("click",sidebarOpen)
 but.addEventListener("click",sidebarOpen)
 
 function sidebarOpen(){
     sidebar.classList.toggle("show")
 }
+
